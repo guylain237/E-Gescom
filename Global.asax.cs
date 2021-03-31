894617be651d932c3d1e4256e8eaef587f8b7593
guylain237 <72466774+guylain237@users.noreply.github.com>
2021-03-31 15:38:55 +0100

    Ajoutez des fichiers projet.

---
+++ Global.asax.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+
+
+// exo : creer un controller account avec quatre action qui mene a quatre vues differentt
+//1 er : login 
+// 2eme : register
+// 3eme : forgot password
+// 4 : terms and condition
+// les contres vues sont interconnectes par des liens sur les pages
+
+namespace Login
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+        protected void Session_End()
+        {
+            FormsAuthentication.SignOut();
+            Response.Redirect(FormsAuthentication.LoginUrl);
+
+        }
+    }
+}
